d1fc95b24a3721e2e06c130fcc1bb5faef9ca826
Шатравский Никита <shatick7975@gmail.com>
2025-10-24 16:58:37 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, make_response, redirect
+from flask import Blueprint, render_template, request, make_response, redirect, session
 
 lab3 = Blueprint('lab3', __name__)
 
@@ -26,7 +26,47 @@ def del_cookie():
 
 @lab3.route('/lab3/form1')
 def form1():
+    errors = {}
     user = request.args.get('user')
     age = request.args.get('age')
     sex = request.args.get('sex')
-    return render_template('lab3/form1.html', user=user, age=age, sex=sex)  
+    
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+    if age == '':
+        errors['age'] = 'Заполните возраст!'
+    
+    return render_template('lab3/form1.html', 
+                         user=user, age=age, sex=sex, 
+                         errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+@lab3.route('/lab3/pay')
+def pay():
+    
+    drink = request.args.get('drink')
+    milk = request.args.get('milk')
+    sugar = request.args.get('sugar')
+    
+    
+    price = 120  
+    if drink == 'black-tea':
+        price = 80
+    elif drink == 'green-tea':
+        price = 70
+    
+    if milk:  
+        price += 30
+    if sugar:  
+        price += 10
+    
+    return render_template('lab3/pay.html', price=price)
+
+@lab3.route('/lab3/success', methods=['POST'])
+def success():
+    price = request.form.get('price')
+    return render_template('lab3/success.html', price=price)
