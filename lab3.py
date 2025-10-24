f1321b97bd03308b938cb6f377d4783d2e375f13
Шатравский Никита <shatick7975@gmail.com>
2025-10-24 15:44:05 +0700

    Заготовка

--- lab3.py
+++ lab3.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, render_template, request, make_response, redirect
+from flask import Blueprint, render_template
 
 lab3 = Blueprint('lab3', __name__)
 
-
 @lab3.route('/lab3/')
 def lab():
-    return "Третья лабораторная работа - в разработке"
+    return render_template('lab3/lab3.html')  
