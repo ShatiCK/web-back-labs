bf684c0598346e09905ec2cfd16e8ea7bf6de0a7
Шатравский Никита <shatick7975@gmail.com>
2025-11-14 23:56:21 +0700

    Арифметика

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from datetime import datetime
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 
 
@@ -28,6 +30,7 @@ def index():
                 <li><a href="''' + url_for('lab1.lab') + '''">Первая лабораторная</a></li>
                 <li><a href="''' + url_for('lab2.lab') + '''">Вторая лабораторная</a></li>
                 <li><a href="''' + url_for('lab3.lab') + '''">Третья лабораторная</a></li>
+                <li><a href="''' + url_for('lab4.lab') + '''">Четвертая лабораторная</a></li>
             </menu>
         </main>
         <footer>
